cc4fc5b06ff21f41b6bc2e5f4b510ab1f56e87a2
hala-samir <h.samir2011@gmail.com>
2022-08-03 15:42:16 +0200

    add new web elemsnts for test

--- src/main/test/tests/LoginEmiratsTest.java
+++ src/main/test/tests/LoginEmiratsTest.java
@@ -5,13 +5,19 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginEmiratsTest extends TestBase{
     HomePage homeObj;
+    EmiratsBenefitsPage emiratsBenefitsPageObj;
     @Test(priority = 1)
     public void checkSuccessfulLogin(){
         homeObj = new HomePage(driver);
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        emiratsBenefitsPageObj = new EmiratsBenefitsPage(driver);
+        //driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         homeObj.clickBenefitsForEmirats();
+        emiratsBenefitsPageObj.clickBuildHomeLink();
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        driver.getPageSource().contains(prop.getProperty("exchange_land_link").toString());
+        emiratsBenefitsPageObj.clickexchangeLandLink();
+        // driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+       // driver.getPageSource().contains(prop.getProperty("exchange_land_link").toString());
+
 
     }
 }
